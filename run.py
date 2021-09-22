d810bb9c1e506b1af5957b41d064ad1d58a6c0ec
Moritz Makowski <moritz@dostuffthatmatters.dev>
2021-09-22 09:31:51 +0000

    Refactor program to also use cli arguments

--- run.py
+++ run.py
@@ -1,4 +1,17 @@
 from src import main
+import json
+import sys
+
+project_dir = "/".join(__file__.split("/")[:-1])
 
 if __name__ == "__main__":
-    main.run()
+
+    # Calling "python run.py 20210909" will ignore the dates from the config
+    if len(sys.argv) == 2 and sys.argv[1].isnumeric() and len(sys.argv[1]) == 8:
+        DATES = [sys.argv[1]]
+    else:
+        with open(f"{project_dir}/config.json", "r") as f:
+            DATES = json.load(f)["dates"]
+
+    for date_string in DATES:
+        main.run(date_string)
--- src/main.py
+++ src/main.py
@@ -17,7 +17,6 @@ data_dir = f"{project_dir}/data"
 with open(f"{project_dir}/config.json", "r") as f:
     config = json.load(f)
     ERA52ARL = config["era52arl"]
-    DATES = config["dates"]
     AREA = config["area"]
 
 
@@ -37,7 +36,7 @@ months = [
 ]
 
 
-def run():
+def run(date_string):
 
     area_string = f"{AREA['north']}/{AREA['west']}/{AREA['south']}/{AREA['east']}"
     cache_dir = f"{project_dir}/cache/{area_string.replace('/', '-')}"
@@ -47,76 +46,74 @@ def run():
         os.makedir(f"{cache_dir}/grib")
         os.makedir(f"{cache_dir}/arl")
 
-    for year in DATES.keys():
-        for month in DATES[year].keys():
-            for day in DATES[year][month]:
-                ymd_string = f"{year}{str(month).zfill(2)}{str(day).zfill(2)}"
+    year, month, day = int(date_string[:4]), int(date_string[4:6]), int(date_string[6:])
+    file_prefix = f"ERA5_{year}.{months[int(month)-1]}{str(day).zfill(2)}"
+    models = {
+        "3d": f"{file_prefix}.3dpl.grib",
+        "2da": f"{file_prefix}.2dpl.all.grib",
+    }
 
-                file_prefix = f"ERA5_{year}.{months[int(month)-1]}{str(day).zfill(2)}"
-                models = {
-                    "3d": f"{file_prefix}.3dpl.grib",
-                    "2da": f"{file_prefix}.2dpl.all.grib",
-                }
+    for model in models:
+        out_file = f"{data_dir}/grib/{models[model]}"
+        cache_file = f"{cache_dir}/grib/{models[model]}"
+        out_cfg = f"era52arl.cfg"
+        cache_cfg = f"{cache_dir}/era52arl.cfg"
 
-                for model in models:
-                    out_file = f"{data_dir}/grib/{models[model]}"
-                    cache_file = f"{cache_dir}/grib/{models[model]}"
-                    out_cfg = f"era52arl.cfg"
-                    cache_cfg = f"{cache_dir}/era52arl.cfg"
+        if os.path.isfile(cache_file) and os.path.isfile(cache_cfg):
+            print(f"{date_string} - using cached grib ({model})")
+            shutil.copy(cache_file, out_file)
+            shutil.copy(cache_cfg, out_cfg)
+        else:
+            print(f"{date_string} - computing grib ({model})")
+            subprocess.run(
+                [
+                    "python3.9",
+                    f"{project_dir}/src/helpers/get_era5_cds.py",
+                    f"--{model}",
+                    "-y",
+                    f"{year}",
+                    "-m",
+                    f"{month}",
+                    "-d",
+                    f"{day}",
+                    "--dir",
+                    f"{data_dir}/grib",
+                    "-g",
+                    "--area",
+                    area_string,
+                ],
+                stderr=subprocess.PIPE,
+                stdout=subprocess.PIPE,
+            )
+            if not os.path.isfile(out_file):
+                print(f"{date_string} - data not available")
+                return
 
-                    if os.path.isfile(cache_file) and os.path.isfile(cache_cfg):
-                        print(f"{ymd_string} - using cached grib ({model})")
-                        shutil.copy(cache_file, out_file)
-                        shutil.copy(cache_cfg, out_cfg)
-                    else:
-                        print(f"{ymd_string} - computing grib ({model})")
-                        subprocess.run(
-                            [
-                                "python3.9",
-                                f"{project_dir}/src/helpers/get_era5_cds.py",
-                                f"--{model}",
-                                "-y",
-                                f"{year}",
-                                "-m",
-                                f"{month}",
-                                "-d",
-                                f"{day}",
-                                "--dir",
-                                f"{data_dir}/grib",
-                                "-g",
-                                "--area",
-                                area_string,
-                            ],
-                            stderr=subprocess.PIPE,
-                            stdout=subprocess.PIPE,
-                        )
-                        assert os.path.isfile(out_file)
-                        shutil.copy(out_file, cache_file)
+            shutil.copy(out_file, cache_file)
+            assert "new_era52arl.cfg" in os.listdir(".")
+            os.rename("new_era52arl.cfg", out_cfg)
+            shutil.copy(out_cfg, cache_cfg)
 
-                        assert "new_era52arl.cfg" in os.listdir(".")
-                        os.rename("new_era52arl.cfg", out_cfg)
-                        shutil.copy(out_cfg, cache_cfg)
+    filename = f"ERA5.{date_string}.ARL"
+    out_file = f"{data_dir}/arl/{filename}"
+    cache_file = f"{cache_dir}/arl/{filename}"
 
-                filename = f"ERA5_{ymd_string}.ARL"
-                out_file = f"{data_dir}/arl/{filename}"
-                cache_file = f"{cache_dir}/arl/{filename}"
-
-                if os.path.isfile(cache_file):
-                    print(f"{ymd_string} - using cached arl")
-                    shutil.copy(cache_file, out_file)
-                else:
-                    print(f"{ymd_string} - computing arl")
-                    subprocess.run(
-                        [
-                            ERA52ARL,
-                            f"-i{data_dir}/grib/{models['3d']}",
-                            f"-a{data_dir}/grib/{models['2da']}",
-                        ],
-                        stderr=subprocess.PIPE,
-                        stdout=subprocess.PIPE,
-                    )
-                    os.rename("DATA.ARL", out_file)
-                    shutil.copy(out_file, cache_file)
+    if os.path.isfile(cache_file):
+        print(f"{date_string} - using cached arl")
+        shutil.copy(cache_file, out_file)
+    else:
+        print(f"{date_string} - computing arl")
+        subprocess.run(
+            [
+                ERA52ARL,
+                f"-i{data_dir}/grib/{models['3d']}",
+                f"-a{data_dir}/grib/{models['2da']}",
+            ],
+            stderr=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+        )
+        os.rename("DATA.ARL", out_file)
+        shutil.copy(out_file, cache_file)
 
     for tmp_file in ["arldata.cfg", "era52arl.cfg", "ERA52ARL.MESSAGE"]:
         if os.path.isfile(tmp_file):
